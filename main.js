d2ce0e5a858fcd35327db8589100a96c4ba134f5
Vladimir Sitnov <vladimirsitnov@gmail.com>
2017-05-19 12:05:15 +0700

    syntax update

--- main.js
+++ main.js
@@ -61,14 +61,14 @@ window.onload = function main()
 	tue.add(Course.create(14*60 + 30, 1*60 + 35, SEMINAR, eng, nov(3151), tambovskaya));
 	//tue.add(Course.create(18*60 + 10, 1*60 + 35, LECTURE, rnd, nsu(313), arkashov));
 
-	//wed.add(Course.create(09*60 +  0, 1*60 + 35, LECTURE, db, iae('УЦ'), islam));
+	//wed.add(Course.create( 9*60 +  0, 1*60 + 35, LECTURE, db, iae('УЦ'), islam));
 	wed.add(Course.create(10*60 + 45, 1*60 + 35, SEMINAR, ood, inp(508), gayazov));
 	wed.add(Course.create(12*60 + 30, 1*60 + 35, LECTURE, dis, inp('Библиотека'), fatkin));
 	
 	//thu.add(Course.create( 9*60 +  0, 3*60 + 25, PRACTICE, tsani, nsu(346), unknown));
 	//thu.add(Course.create(12*60 + 40, 1*60 + 35, LECTURE, tvms, nsu('БА'), kovalevsky));
 
-	fri.add(Course.create(09*60 +  0, 1*60 + 35, PRACTICE,  db,  inp(508), pirogov));
+	fri.add(Course.create( 9*60 +  0, 1*60 + 35, PRACTICE,  db,  inp(508), pirogov));
 	fri.add(Course.create(10*60 + 45, 1*60 + 35, LECTURE,  ood,  iae('УЦ'), maginsky));
 	fri.add(Course.create(12*60 + 30, 1*60 + 35, LECTURE,  evm,  inp(508), korol));
 	fri.add(Course.create(14*60 + 15, 1*60 + 35, PRACTICE,  evm,  inp(508), korol));
